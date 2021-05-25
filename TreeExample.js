679fb3f2735415fced0c14b9b6d59da92d67b8e0
Himanshu Jangid <iamahalofan@gmail.com>
2021-05-25 18:21:22 +0530

    very bad example

---
+++ TreeExample.js
@@ -0,0 +1,32 @@
+/**
+ * @author Himanshu Jangid 
+ * @himanshurajora
+ * @summary
+ * Chatbot Tree
+ * 
+ */
+class Tree{
+    /** 
+     * @param {string} question 
+     */
+    constructor(question){
+        this.question = question
+        this.yes = null
+        this.no = null
+    }
+
+    /**
+     * 
+     * @param {number} value value for the new node to add
+     * @returns {Tree} the new tree formed by insertion  
+     */
+
+    insertChild(question, side){
+        const newQuestion = new Tree(question);
+        this[side] = side;
+        return newQuestion;
+    }
+
+}
+
+
